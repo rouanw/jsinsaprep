2fa85ddd34cc5081bb49b9882b2e1d6be70741c8
Rouan Wilsenach <rouanw@gmail.com>
2014-05-29 14:46:09 +0200

    posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blog2::Application.routes.draw do
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
